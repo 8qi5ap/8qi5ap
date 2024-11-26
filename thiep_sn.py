6c6a684cdcbe0b68fb90da96bd7f3cd6be3543a1
8qi5ap <nguyentuongbach.1003@gmail.com>
2024-11-26 20:46:16 +0700

    Create thiep_sn.py
    lụm nha thầy

---
+++ thiep_sn.py
@@ -0,0 +1,5 @@
+from guizero import App, Text,Picture
+app=App(title="teo tèo teo téo tèo teo tằng tằng tằng")
+message=Text(app,text="chúc mừng sinh nhật")
+image=Picture(app,image="6.png",width=600, height=900)
+app.display()
